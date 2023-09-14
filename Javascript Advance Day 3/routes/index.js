adb2b197a20c28ce320859e49d29f45327c2d8ff
zettacamp-Hidayat-Tresnadi <hidayat.tresnadi@zettacamp.pro>
2023-09-14 15:50:34 +0700

    add javascript advance final task

--- Javascript Advance Day 3/routes/index.js
+++
@@ -1,7 +0,0 @@
-const express = require('express');
-const bookPurchasing =require('./bookPurchasing')
-const router = express.Router();
-
-router.use('/bookPurchasing',bookPurchasing)
-
-module.exports=router
