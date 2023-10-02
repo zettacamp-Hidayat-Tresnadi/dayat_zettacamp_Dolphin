dd80af5853e5645939f97ed47e0139728ffb1a31
zettacamp-Hidayat-Tresnadi <hidayat.tresnadi@zettacamp.pro>
2023-10-02 17:48:10 +0700

    Add Graphql -Day-3 Folder only

--- MongoDB Day-6/routes/author.js
+++
@@ -1,8 +0,0 @@
-const express = require('express');
-const AuthorController = require('../controller/authorController');
-const router = express.Router();
-
-router.post('/',AuthorController.createAuthor)
-router.get('/',AuthorController.getAllAuthors)
-
-module.exports=router
