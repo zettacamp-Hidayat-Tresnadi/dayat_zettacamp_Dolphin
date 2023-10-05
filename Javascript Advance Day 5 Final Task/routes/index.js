c942551bbc2d20654883f57411e8855ec01a70e3
zettacamp-Hidayat-Tresnadi <hidayat.tresnadi@zettacamp.pro>
2023-10-05 16:20:54 +0700

    Add Folder Moment JS Day-1 Folder

--- Javascript Advance Day 5 Final Task/routes/index.js
+++
@@ -1,9 +0,0 @@
-const express = require('express');
-const songList =require('./songList')
-const login = require('./login')
-const router = express.Router();
-
-router.use('/songList',songList)
-router.use('/login',login)
-
-module.exports=router
