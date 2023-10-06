e5ce1d507def15fdc7a86edacaa6b68947148c72
zettacamp-Hidayat-Tresnadi <hidayat.tresnadi@zettacamp.pro>
2023-10-06 16:31:27 +0700

    Add Moment-JS-Day-2 Folder

--- Javascript Advance Day 5 Final Task/routes/login.js
+++
@@ -1,7 +0,0 @@
-const express = require('express');
-const router = express.Router();
-const { UserController } = require('../controller/userController');
-
-router.post('/', UserController.login)
-
-module.exports = router
