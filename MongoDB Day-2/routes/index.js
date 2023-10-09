16c0d696a89f9e5b48dcc8f1e28a3e990f402ba8
zettacamp-Hidayat-Tresnadi <hidayat.tresnadi@zettacamp.pro>
2023-10-09 23:40:31 +0700

    Add Moment JS Day-3 Folder only

--- MongoDB Day-2/routes/index.js
+++
@@ -1,9 +0,0 @@
-const express = require('express');
-const bookPurchasing =require('./bookPurchasing')
-const author =require('./author')
-const router = express.Router();
-
-router.use('/bookPurchasing',bookPurchasing)
-router.use('/authors',author)
-
-module.exports=router
